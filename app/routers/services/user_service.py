8f8d1bd396553decc1f0a355082fdc5d54b6dc79
Hooghir <hooghirm29@gmail.com>
2025-12-16 23:11:30 -0500

    Add user service with create_user function

--- app/routers/services/user_service.py
+++ app/routers/services/user_service.py
@@ -1,5 +1,20 @@
 import sqlite3
 
-class creatte_user:
-    connection=sqlite3.connect('app/schemas/user.py')
+
+def create_user(name, email, created_at):
+
+    # Connect to the SQLite database 
+    connection=sqlite3.connect('MoodMind.db')  
     cursor=connection.cursor()
+
+   # Enable foreign key support
+    cursor.execute("""
+    PRAGMA foreign_keys = ON;
+    """)
+
+    cursor.execute('INSERT INTO users (name, email, created_at) VALUES (?, ?, ?)', (name, email, created_at))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
