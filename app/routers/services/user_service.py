12a0b31780536a2cebbe42d700348cb522fe75ed
Hooghir <hooghirm29@gmail.com>
2025-12-22 21:59:46 -0500

    Finished user_service.py. May need to fix the return structure

--- app/routers/services/user_service.py
+++ app/routers/services/user_service.py
@@ -18,3 +18,52 @@ def create_user(name, email, created_at):
     cursor.close()
     connection.close()
 
+def get_user_by_id(user_id):
+
+    # Connect to the SQLite database 
+    connection=sqlite3.connect('MoodMind.db')  
+    cursor=connection.cursor()  
+
+    # Enable foreign key support
+    cursor.execute("""
+    PRAGMA foreign_keys = ON;
+    """)
+
+    cursor.execute('select * from users where id = ?', (user_id,))
+    row = cursor.fetchone()
+
+    if row == None:
+            return None
+    
+    cursor.close()
+    connection.close()
+    return row
+
+def get_user_by_email(email):
+
+    # Connect to the SQLite database 
+    connection=sqlite3.connect('MoodMind.db')  
+    cursor=connection.cursor()  
+
+    # Enable foreign key support
+    cursor.execute("""
+    PRAGMA foreign_keys = ON;
+    """)
+
+    cursor.execute('select * from users where email = ?', (email,))
+    row = cursor.fetchone()
+
+    if row == None:
+            return None
+    
+    cursor.close()
+    connection.close()
+    return row
+    
+ 
+
+    
+
+
+    
+
